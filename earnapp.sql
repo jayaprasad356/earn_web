f2e96eca66759aeae68a3741fc4dc76b79c6a2dc
jayaprasad356 <jayaprasad356@gmail.com>
2022-04-16 09:03:51 +0530

    earnapp sql added

--- earnapp.sql
+++ earnapp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 13, 2022 at 09:12 PM
+-- Generation Time: Apr 16, 2022 at 05:32 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.4.27
 
@@ -31,11 +31,29 @@ CREATE TABLE `daily_income` (
   `id` int(11) NOT NULL,
   `user_id` int(11) DEFAULT NULL,
   `plan_id` int(11) DEFAULT NULL,
+  `purchased_id` int(11) NOT NULL,
   `credited_amount` double DEFAULT NULL,
   `credited_date` text DEFAULT NULL,
   `last_updated` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
   `date_created` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `daily_income`
+--
+
+INSERT INTO `daily_income` (`id`, `user_id`, `plan_id`, `purchased_id`, `credited_amount`, `credited_date`, `last_updated`, `date_created`) VALUES
+(55, 13, 1, 1, 30, '2022-04-15', NULL, '2022-04-15 08:14:58'),
+(56, 13, 2, 2, 65, '2022-03-11', NULL, '2022-04-15 08:14:58'),
+(57, 13, 2, 2, 65, '2022-03-12', NULL, '2022-04-15 08:14:58'),
+(58, 13, 2, 2, 65, '2022-03-13', NULL, '2022-04-15 08:14:58'),
+(59, 13, 2, 2, 65, '2022-03-14', NULL, '2022-04-15 08:14:58'),
+(60, 13, 2, 2, 65, '2022-03-15', NULL, '2022-04-15 08:14:58'),
+(61, 13, 2, 2, 65, '2022-03-16', NULL, '2022-04-15 08:14:58'),
+(62, 13, 2, 2, 65, '2022-03-17', NULL, '2022-04-15 08:14:58'),
+(63, 13, 2, 2, 65, '2022-03-18', NULL, '2022-04-15 08:14:58'),
+(64, 13, 2, 2, 65, '2022-03-19', NULL, '2022-04-15 08:14:58'),
+(65, 13, 2, 2, 65, '2022-03-20', NULL, '2022-04-15 08:14:58');
 
 -- --------------------------------------------------------
 
@@ -92,8 +110,9 @@ CREATE TABLE `purchased_plans` (
 --
 
 INSERT INTO `purchased_plans` (`id`, `user_id`, `plan_id`, `daily_income`, `price`, `valid`, `start_date`, `end_date`, `last_updated`, `date_created`) VALUES
-(6, 13, 1, 30, 600, '90', '2022-04-13', '2022-07-12', NULL, '2022-04-13 17:38:16'),
-(7, 13, 1, 30, 600, '90', '2022-04-13', '2022-07-12', NULL, '2022-04-13 17:46:01');
+(1, 13, 1, 30, 600, '90', '2022-04-14', '2022-07-14', '2022-04-15 06:21:34', '2022-04-15 04:33:44'),
+(2, 13, 2, 65, 1200, '10', '2022-03-10', '2022-07-14', '2022-04-15 07:54:27', '2022-04-15 06:25:01'),
+(3, 13, 1, 30, 600, '90', '2022-04-15', '2022-07-14', NULL, '2022-04-15 09:15:09');
 
 -- --------------------------------------------------------
 
@@ -124,7 +143,12 @@ INSERT INTO `recharges` (`id`, `user_id`, `amount`, `status`, `payment_type`, `l
 (7, 13, 500, 1, 'upi', '2022-04-13 14:07:48', '2022-04-13 14:07:20'),
 (8, 13, 500, 1, 'paytm', NULL, '2022-04-13 17:42:37'),
 (9, 13, 500, 1, 'paytm', NULL, '2022-04-13 17:44:18'),
-(10, 13, 500, 1, 'paytm', NULL, '2022-04-13 17:44:28');
+(10, 13, 500, 1, 'paytm', NULL, '2022-04-13 17:44:28'),
+(11, 13, 200, 1, 'upi', NULL, '2022-04-15 04:33:37'),
+(12, 13, 500, 1, 'paytm', NULL, '2022-04-15 06:24:40'),
+(13, 13, 700, 1, 'paytm', NULL, '2022-04-15 06:24:56'),
+(14, 13, 500, 1, 'paytm', NULL, '2022-04-15 09:02:32'),
+(15, 13, 500, 1, 'paytm', NULL, '2022-04-15 09:14:54');
 
 -- --------------------------------------------------------
 
@@ -139,6 +163,7 @@ CREATE TABLE `users` (
   `referral` text DEFAULT NULL,
   `my_refer_code` varchar(28) DEFAULT NULL,
   `balance` double DEFAULT NULL,
+  `earn` double NOT NULL,
   `last_updated` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
   `date_created` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -147,11 +172,12 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `mobile`, `referral`, `my_refer_code`, `balance`, `last_updated`, `date_created`) VALUES
-(11, 'Aravind', '9025635524', '2002', 'I1I2BSDBSD', 0, '2022-04-13 16:49:45', '2022-04-05 16:09:21'),
-(12, 'Bala', '9055635524', 'I1I2BSDBSD', 'DDI2BAABSD', 0, '2022-04-06 14:43:42', '2022-04-05 16:09:21'),
-(13, 'Prasad', '8778624681', 'I1I2BSDBSD', 'JAGPXA1FE6', 400, '2022-04-13 17:46:01', '2022-04-06 14:33:53'),
-(14, 'Surya', '9090909090', 'JAGPXA1FE6', 'CPP3HK8Y8U', NULL, NULL, '2022-04-13 15:07:18');
+INSERT INTO `users` (`id`, `name`, `mobile`, `referral`, `my_refer_code`, `balance`, `earn`, `last_updated`, `date_created`) VALUES
+(11, 'Aravind', '9025635524', '2002', 'I1I2BSDBSD', 0, 0, '2022-04-13 16:49:45', '2022-04-05 16:09:21'),
+(12, 'Bala', '9055635524', 'I1I2BSDBSD', 'DDI2BAABSD', 0, 0, '2022-04-06 14:43:42', '2022-04-05 16:09:21'),
+(13, 'Prasad', '8778624681', 'I1I2BSDBSD', 'JAGPXA1FE6', 400, 680, '2022-04-15 09:15:09', '2022-04-06 14:33:53'),
+(14, 'Surya', '9090909090', 'JAGPXA1FE6', 'CPP3HK8Y8U', 0, 0, '2022-04-15 08:07:49', '2022-04-13 15:07:18'),
+(15, 'Rahul', '9638527410', 'THUsbsj', 'KP51YU4WTR', NULL, 0, NULL, '2022-04-15 09:14:36');
 
 --
 -- Indexes for dumped tables
@@ -195,7 +221,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `daily_income`
 --
 ALTER TABLE `daily_income`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
 
 --
 -- AUTO_INCREMENT for table `plans`
@@ -207,19 +233,19 @@ ALTER TABLE `plans`
 -- AUTO_INCREMENT for table `purchased_plans`
 --
 ALTER TABLE `purchased_plans`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `recharges`
 --
 ALTER TABLE `recharges`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
